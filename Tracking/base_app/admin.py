554e0795054610df360d971007a0e10a44839ac1
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-08-25 21:26:59 +0300

    Создали модели,  зарегистрировали из в админке, выполнили миграции и создали суперпользователя

--- Tracking/base_app/admin.py
+++ Tracking/base_app/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import *
+from django.utils.html import format_html, mark_safe
 
 # Register your models here.
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'show_photo')
+    fields = ['name', 'email', ('phone', 'photo', 'show_photo')]
+    readonly_fields = ["show_photo"]
+
+    def show_photo(self, obj):
+        if obj.photo:
+            return format_html(
+                f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+            # можно и с использованием функции mark_safe
+            # return mark_safe(
+            # f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+        else:
+            return "Фото не доступно"
+
+    show_photo.short_description = 'Фото клиента'
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'client', 'product', 'category', 'show_photo', 'order_date',
+                    'order_amount', 'status', 'completed')
+    list_filter = ('order_number', 'client')    
+    readonly_fields = ["show_photo"]
+
+    def show_photo(self, obj):
+        if obj.photo:
+            return format_html(
+                f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+            # можно и с использованием функции mark_safe
+            # return mark_safe(
+            # f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+        else:
+            return "Фото не доступно"
+
+    show_photo.short_description = 'Фото товара'    
--- Tracking/base_app/models.py
+++ Tracking/base_app/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+
+class Client(models.Model):
+    objects = None
+    name = models.CharField(max_length=100, verbose_name='Имя клиента')
+    email = models.EmailField(verbose_name='Email')
+    phone = models.CharField(max_length=15, verbose_name='Телефон')
+    photo = models.ImageField(upload_to='images',
+                              help_text="Введите фото",
+                              verbose_name="Фото клиента",
+                              null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+
+class Order(models.Model):
+    objects = None
+    order_number = models.CharField(max_length=100, verbose_name='Номер заказа')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='orders', verbose_name='Клиент')
+    product = models.CharField(max_length=100, verbose_name="Товар")
+    CATEGORY_CHOICES = [
+        ('Процессоры', 'Процессоры'),
+        ('Материнские платы', 'Материнские платы'),
+        ('Видеокарты', 'Видеокарты'),
+        ('Оперативная память', 'Оперативная память'),
+        ('SSD-диски', 'SSD-диски'),
+        ('HDD-диски', 'HDD-диски'),
+        ('Блоки питания>', 'Блоки питания'),
+    ]
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, verbose_name='Категория')
+    photo = models.ImageField(upload_to='images',
+                              help_text="Введите фото",
+                              verbose_name="Фото товара",
+                              null=True, blank=True)
+    
+    order_date = models.DateField(verbose_name='Дата заказа')
+    order_amount = models.DecimalField(verbose_name="Сумма заказа", max_digits=10, decimal_places=2)
+    ORDER_STATUS_CHOICES = [
+        ('Поступил', 'Поступил'),
+        ('Выполняется', 'Выполняется'),
+        ('Закрыт', 'Закрыт'),
+    ]
+    status = models.CharField(max_length=50, choices=ORDER_STATUS_CHOICES, verbose_name='Статус')
+    completed = models.BooleanField(default=False, verbose_name='Заказ завершен')
+    completion_date = models.DateField(null=True, blank=True, verbose_name='Дата завершения')
+
+
+    def __str__(self):
+        return f'{self.order_number} {self.client}'
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+@receiver(pre_save, sender=Order)
+def update_completion_date(sender, instance, **kwargs):
+    if instance.completed and not instance.completion_date:
+        instance.completion_date = instance.date_created
